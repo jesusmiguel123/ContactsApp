45b3d1c6233226213938e5d061dbe8dbc7b8ee99
Jesus Yacolca <jesus.yacolca17@gmail.com>
2023-03-01 18:28:47 -0500

    Final Commit

--- backend/app/db.py
+++ backend/app/db.py
@@ -4,7 +4,20 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine("postgresql+psycopg2://pguser:asd123@db:5432/app")
+from dotenv import load_dotenv
+import os
+
+load_dotenv("../.env")
+
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+POSTGRES_HOST = os.getenv("POSTGRES_HOST")
+POSTGRES_PORT = os.getenv("POSTGRES_PORT")
+POSTGRES_DB = os.getenv("POSTGRES_DB")
+
+str_connection = f"postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
+
+engine = create_engine(str_connection)
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
                                          bind=engine))
