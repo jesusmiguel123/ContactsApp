e3525d0935c735e740ccd03830122f566420532a
Jesus Yacolca <jesus.yacolca17@gmail.com>
2023-03-09 21:59:24 -0500

    Final Commit

--- frontend/src/Components/User/Profile.jsx
+++ frontend/src/Components/User/Profile.jsx
@@ -37,28 +37,26 @@ const Profile = ({ user }) => {
       }
    };
 
+   const editIcon = <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-pencil edit" viewBox="0 0 16 16"><path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/></svg>;
+
    return (
       <div className="profile">
          <h1>{user}</h1>
-         <table>
-            <tbody>
-               <tr className="row1">
-                  <td colSpan="4">
-                     <img src={photo} alt={user}/>
-                  </td>
-               </tr>
-               <tr>
-                  <th scope="row" id="field-name-profile">Name</th>
-                  <td>{data.name}</td>
-                  <th scope="row" id="field-name-profile">Lastname</th>
-                  <td>{data.lastname}</td>
-               </tr>
-               <tr className="row1">
-                  <th scope="row" colSpan="2">Email</th>
-                  <td colSpan="2">{data.email}</td>
-               </tr>
-            </tbody>
-         </table>
+         <div className="data">
+            <div className="data-image">
+               <img src={photo} alt={user}/>
+               {editIcon}
+            </div>
+            <div className="data-fields">
+               <div className="name-lastname">
+                  <div><p>Name:</p> {data.name} {editIcon}</div>
+                  <div><p>Lastname:</p> {data.lastname} {editIcon}</div>
+               </div>
+               <div className="email">
+                  <div><p>Email:</p> {data.email} {editIcon}</div>
+               </div>
+            </div>
+         </div>
       </div>
    );  
 }
