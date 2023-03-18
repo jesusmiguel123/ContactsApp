9ed0c386dc3f7ef115f134f0ae3bc2bd16b4abfe
Jesus Yacolca <jesus.yacolca17@gmail.com>
2023-03-18 14:32:30 -0500

    Final Commit

--- backend/deploy/app.sh
+++ backend/deploy/app.sh
@@ -2,4 +2,21 @@
 
 flask check-db
 flask init-db
-flask run --host 0.0.0.0 --debug
+flask initial-admin
+
+NAME="app"
+FLASK_DIR=$(dirname $(cd `dirname $0` && pwd))
+LOG_LEVEL=debug
+USER=app
+GROUP=app
+NUM_WORKERS=5
+DJANGO_WSGI_MODULE=config.wsgi
+
+gunicorn ${DJANGO_WSGI_MODULE}:application \
+   --bind 0.0.0.0:8000 \
+   --name $NAME \
+   --worker-connections 1000 \
+   --workers $NUM_WORKERS \
+   --user $USER \
+   --group $GROUP \
+   --log-level $LOG_LEVEL
